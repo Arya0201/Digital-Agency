415cfa8233c606a22010dcc21151b4179c1e75c6
Arya patel <aryapatel1119@gmail.com>
2022-06-28 10:13:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { SliderComponent } from './slider/slider.component';
+import { NextDirective } from './next.directive';
+import { PrevDirective } from './prev.directive';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SliderComponent,
+    NextDirective,
+    PrevDirective,
+    SectionComponent,
+    FooterComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
